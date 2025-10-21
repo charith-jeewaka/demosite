16fbd362967398bdaffef245c06d2271b2a57bb5
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2025-10-22 01:38:35 +0530

    mobile responsive v0.6 navigation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ window.addEventListener('load', function () {
     preloader.style.transition = 'opacity 0.5s ease';
     setTimeout(() => preloader.style.display = 'none', 500);
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
